ebd7b570c0acc2b771d0f541e80c3478070fddf9
theodoreyapi <theodoreyapi93@gmail.com>
2026-06-23 19:36:28 +0000

    version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 5
-        versionName = "2.0.1"
+        versionCode = 6
+        versionName = "2.1.0"
     }
 
     signingConfigs {
